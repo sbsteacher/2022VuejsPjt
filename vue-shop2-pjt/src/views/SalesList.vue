cfc7ca111e936ae6d2d80455cf639131bacdf535
ParkDoheum <pirbak@gmail.com>
2022-07-21 11:30:43 +0900

    0721 - 2교시

--- vue-shop2-pjt/src/views/SalesList.vue
+++ vue-shop2-pjt/src/views/SalesList.vue
@@ -10,7 +10,8 @@
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th></th>
+            <th>              
+            </th>
             <th>제품명</th>
             <th>제품가격</th>
             <th>배송비</th>
@@ -20,7 +21,11 @@
         </thead>
         <tbody>
           <tr v-for="(product, idx) in productList" :key="product.id">
-            <td></td>
+            <td>
+              <img v-if="product.path !== null" 
+                  :src="`/static/img/${product.id}/1/${product.path}`" 
+                  style="height:50px; width:auto;">
+            </td>
             <td>{{ product.product_name }}</td>
             <td>{{ product.product_price }}</td>
             <td>{{ product.delivery_price }}</td>
@@ -35,7 +40,7 @@
               <router-link class="nav-link" :to="{ path: '/update', query: {product_id: product.id} }">
                 <button type="button" class="btn btn-warning me-1">수정</button>
               </router-link>              
-              <button type="button" class="btn btn-danger">삭제</button>
+              <button type="button" class="btn btn-danger" @click="deleteProduct(product.id)">삭제</button>
             </td>
           </tr>
         </tbody>
@@ -59,7 +64,10 @@ export default {
     goToImageInsert(idx) {
       this.$store.commit('sallerSelectedProduct', this.productList[idx]);
       this.$router.push( {path: '/image_insert'} );
-    }
+    },
+    deleteProduct(productId) {
+      console.log(productId);
+    },
   },
   created() {
     this.getProductList();
